6192fcab987ae09b26dc19897998170fff51cce4
Seungyoon Song <rosekalun@gmail.com>
2023-11-13 11:51:10 +0900

    first commit

---
+++ src/test/java/com/example/PillSu/PillSuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PillSu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillSuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
